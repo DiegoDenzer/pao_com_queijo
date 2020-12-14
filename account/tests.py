e545934c58c3923fa0d64f0057015510608c05d3
diegodenzer <lovesdd291210>
2020-12-14 17:34:27 -0300

    Testes passando. Close #22

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+def test_login_page(client):
+    resp = client.get('/account/login/')
+    assert resp.status_code == 200
+
+
+def test_register_page(client):
+    resp = client.get('/account/register')
+    assert resp.status_code == 200
--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,5 @@ from account.views import Register, cadastrar_usuario
 from base.views import home_page
 
 urlpatterns = [
-    # path('register', Register.as_view(), name='register'),
-    path('register2', cadastrar_usuario, name='register'),
+    path('register', cadastrar_usuario, name='register'),
 ]
