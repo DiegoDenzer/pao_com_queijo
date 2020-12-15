2a6781f8f5d67bb1f283ed958a52a889532df660
diegodenzer <lovesdd291210>
2020-12-15 15:01:03 -0300

    Mekemigrations no heroku
    close #26

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from account.views import Register, cadastrar_usuario
-from base.views import home_page
+from account.views import cadastrar_usuario
 
 urlpatterns = [
     path('register', cadastrar_usuario, name='register'),
